7a9fcd68cbb5d36bb5fcd7a62b5efed1b2c0b785
devsajibghosh <devsajibghosh@gmail.com>
2024-07-03 14:43:51 +0530

    basic of oop-1-module

---
+++ Readme.md
@@ -0,0 +1,24 @@
+1. Programming has two way : 1.procudureal and 2.non-procudureal
+________________________________________________________________
+#Procedural Aspect --- Laravel Framework
+________________________________________________________________
+#What is class & object-- class: rebember-- Person
+________________________________________________________________
+@object means---- hand leg head hair and air 
+________________________________________________________________
+$$ overall --> Person🤵🏻 is Class and His element is object👋🏻
+________________________________________________________________
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ oop/basic_oop.php
@@ -0,0 +1,48 @@
+<?php 
+
+
+Class House{
+
+  // properties
+  public $name,$room,$floor;
+  // methodes build 
+
+  public function nam_info($name,$room,$floor){
+
+    $this->name = $name;
+    $this->room = $room;
+    $this->floor = $floor;
+
+ }
+
+ function get_name(){
+  return $this->name;
+ }
+ function get_room(){
+  return $this->room;
+ }
+ function get_floor(){
+  return $this->floor;
+ }
+
+}
+
+$house = new House();  /*  Class to Object -- here is a class of house and create a object $house to be a object */
+
+// assing the value of object => "";
+$house->name = "Sajib Ghosh <br>";
+$house->room = " 10 room in this house<br>";
+$house->floor = "I love 2nd floor <br>";
+// print of object
+echo $house->get_name();
+echo $house->get_room();
+echo $house->get_floor();
+
+
+
+
+
+
+
+
+?>
